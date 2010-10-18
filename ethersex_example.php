f67cbdf33b4babfd75f367c78f1fa5b3ea05047e
arazer <arazer1991@googlemail.com>
2010-10-18 18:33:28 +0200

    modified:   ethersex_example.php

--- ethersex_example.php
+++ ethersex_example.php
@@ -3,6 +3,34 @@
 define(IP, 'hs.flipdot.de'); // deine ethersex ip adresse
 define(PORT, 2701); // standart port im image
 
+function init() //initializing esex board goes here
+{
+
+}
+function setDDRhex($port, $hex) //writes register hex value
+{
+	request("io set ddr" . $port . $hex); 
+}
+
+function setDDR($port, $pin, $value) //writes register value masked
+{
+	if($value != 0) $value="ff";
+	$hex = dechex(2^$pin); //TODO exponentialfkt korrekt?
+	request("io set ddr" . $port . " " . $value . " " . $hex); 
+}
+
+function setPorthex($port, $hex)
+{
+	request("io set port" . $port . $hex);
+}
+
+function setPort($port, $pin, $value)
+{
+	if($value != 0) $value="ff";
+        $hex = dechex(2^$pin); //TODO exponentialfkt korrekt?
+        request("io set port" . $port . " " . $value . " " . $hex);	
+}
+
 request("1w convert");
 
 $response = request("1w list");
